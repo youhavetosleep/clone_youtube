d7468485576a08dd5fecb1ab6132a82691dc18eb
JUNGSU JIN <jungsujin@JUNGSUui-iMac.local>
2021-09-02 14:33:19 +0900

    make video item

--- src/compnents/video_item/video_item.jsx
+++ src/compnents/video_item/video_item.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-         <h1>{props.video.snippet.title}</h1>
-    );
+const VideoItem = (props) => {
+    const {title, thumbnails, channelTitle} = props.video.snippet
+    return (
+    <li className= {styles.container}>
+        <div className={styles.video}>
+            <img className= {styles.thumbnails} src={thumbnails.medium.url} alt='video thumbnails' />
+            <div className={styles.metadata}>
+                <p className= {styles.title} >{title}</p>
+                <p className= {styles.channel}>{channelTitle}</p>
+            </div>
+        </div>
+    </li>
+)};
 
-export default VideoItem;
+export default VideoItem; 
